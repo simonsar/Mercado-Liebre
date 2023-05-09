ab2733e32ae3b2d9ca984f9666249d5270e9eba7
simonsar <simonsara989@gmail.com>
2023-05-09 12:46:41 -0300

    casi terminado

--- app.js
+++ app.js
@@ -11,3 +11,20 @@ app.listen(3000, () => {
 app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/views/home.html')
 })
+
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + '/views/register.html')
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + '/views/login.html')
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/home.html')
+});
+
+app.post("/", (req, res) => {
+  res.sendFile(__dirname + '/views/home.html')
+});
